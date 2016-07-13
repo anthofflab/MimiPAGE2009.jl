abb06a652accb997c3fa6df761cf23d8cb7b1c3b
Frances Moore <fmoore@ucdavis.edu>
2016-07-13 12:58:00 -0700

    CO2 radiative forcing component

---
+++ src/CO2forcing.jl
@@ -0,0 +1,17 @@
+using Mimi
+
+@defcomp co2forcing begin
+    c_CO2concentration=Parameter(index=[time],unit="ppbv")
+    f0_CO2baseforcing=Parameter(unit="W/m2")
+    fslope_CO2forcingslope=Parameter(unit="W/m2")
+    c0_baseCO2conc=Parameter(unit="ppbv")
+    f_CO2forcing=Variable(index=[time],unit="W/m2")
+end
+
+function run_timestep(s::co2forcing, t::Int64)
+    v = s.Variables
+    p = s.Parameters
+
+    #eq.13 in Hope 2006
+    v.f_CO2forcing[t]=p.f0_CO2baseforcing+p.fslope_CO2forcingslope*log(p.c_CO2concentration[t]/p.c0_baseCO2conc)
+end    
