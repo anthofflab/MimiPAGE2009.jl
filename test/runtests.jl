2f7ffe94a1a90a69356044f2e5701960245531a0
Frances Moore <fmoore@ucdavis.edu>
2016-08-02 16:56:30 -0700

    CH4 radiative forcing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-tests = ["ClimateTemperature","CH4cycle","CO2emissions","CH4emissions","CO2forcing","N2Oemissions","N2Oforcing","N2Ocycle"]
+tests = ["ClimateTemperature","CH4cycle","CO2emissions","CH4emissions","CO2forcing","N2Oemissions","N2Oforcing","N2Ocycle","CH4forcing"]
 
 for t in tests
     fp = joinpath("test_$t.jl")
