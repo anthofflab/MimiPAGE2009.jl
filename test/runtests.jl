27ef5f5c61436a01dfe75e579c5c9870973e9927
James Rising <jarising@gmail.com>
2016-08-02 19:53:16 -0400

    merged in master, added test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-tests = ["ClimateTemperature","CH4cycle","CO2emissions","CH4emissions","CO2forcing","N2Oemissions","N2Oforcing","N2Ocycle"]
+tests = ["ClimateTemperature","CH4cycle","CO2emissions","CH4emissions","CO2forcing","N2Oemissions","N2Oforcing","N2Ocycle", "TotalForcing"]
 
 for t in tests
     fp = joinpath("test_$t.jl")
