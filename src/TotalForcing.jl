59e0d08727fbda0872540748d2206a87afc8532f
James Rising <jarising@gmail.com>
2016-08-05 10:46:26 -0400

    add on linear gas forcing

--- src/TotalForcing.jl
+++ src/TotalForcing.jl
@@ -4,6 +4,7 @@ using Mimi
     f_CO2forcing = Parameter(index=[time], unit="W/m2")
     f_CH4forcing = Parameter(index=[time], unit="W/m2")
     f_N2Oforcing = Parameter(index=[time], unit="W/m2")
+    f_lineargasforcing = Parameter(index=[time], unit="W/m2")
 
     ft_totalforcing = Variable(index=[time], unit="W/m2")
 end
@@ -11,6 +12,6 @@ end
 function run_timestep(s::TotalForcing, tt::Int64)
     v, p, d = getvpd(s)
 
-    v.ft_totalforcing[tt] = p.f_CO2forcing[tt] + p.f_CH4forcing[tt] + p.f_N2Oforcing[tt]
+    v.ft_totalforcing[tt] = p.f_CO2forcing[tt] + p.f_CH4forcing[tt] + p.f_N2Oforcing[tt] + p.f_lineargasforcing[tt]
 end
 
--- test/test_TotalForcing.jl
+++ test/test_TotalForcing.jl
@@ -11,7 +11,8 @@ totalforcing = addcomponent(m, TotalForcing)
 totalforcing[:f_CO2forcing] = ones(10)
 totalforcing[:f_CH4forcing] = 2*ones(10)
 totalforcing[:f_N2Oforcing] = 4*ones(10)
+totalforcing[:f_lineargasforcing] = 8*ones(10)
 
 run(m)
 
-@test m[:TotalForcing, :ft_totalforcing] == 7*ones(10)
+@test m[:TotalForcing, :ft_totalforcing] == 15*ones(10)
