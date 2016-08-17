841f88e76d32f10a9a1b0df820f4747285ea8d00
James Rising <jarising@gmail.com>
2016-08-16 17:19:58 -0700

    switch to the param names

--- calibration/excel2csv.jl
+++ calibration/excel2csv.jl
@@ -6,28 +6,37 @@ datadir = joinpath(dirname(@__FILE__), "..", "data")
 
 data = readxlsheet(DataFrame, joinpath(datadir, "regioninfo.xlsx"), "Sheet1")
 
-names = Dict(:area => "Area",
+columns = Dict(:area => "Area",
              :gdp => "GDP",
              :population => "Pop",
-             :co2emit => "CO2 emit",
-             :ch4emit => "CH4 emit",
-             :n2oemit => "N2O emit",
-             :linemit => "Lin emit",
-             :semit => "S emit",
-             :naturals => "Natural S",
-             :rt => "RT",
-             :latitude => "Latitude")
+             :e0_baselineCO2emissions => "CO2 emit",
+             :e0_baselineCH4emissions => "CH4 emit",
+             :e0_baselineN2Oemissions => "N2O emit",
+             :e0_baselineLGemissions => "Lin emit",
+             :se0_sulphateemissionsbase => "S emit",
+             :nf_naturalsfx => "Natural S",
+             :rtl_0_realizedtemperature => "RT",
+             :lat_latitude => "Latitude")
 
-for key in keys(names)
+for key in keys(columns)
     println(key)
-    unit = data[1, symbol(names[key])]
+    unit = data[1, symbol(columns[key])]
+    if unit == "Mtonne" || unit == "TgS"
+        unit = "Mtonne/year"
+    end
+    if unit == "Tg/km2"
+        unit = "Tg/km^2/yr"
+    end
+    if unit == "degree"
+        unit = "degreeLatitude"
+    end
 
     open(joinpath(datadir, "$key.csv"), "w") do fp
         write(fp, "# Index: region\n")
         write(fp, "# Unit: $unit\n")
-        write(fp, "Region,\"$(names[key])\"\n")
+        write(fp, "Region,\"$(columns[key])\"\n")
 
-        df = DataFrame(region=data[2:end, symbol("Abbr.")], value=data[2:end, symbol(names[key])])
+        df = DataFrame(region=data[2:end, symbol("Abbr.")], value=data[2:end, symbol(columns[key])])
         writedlm(fp, convert(DataMatrix, df), ",", header=false)
     end
 end
