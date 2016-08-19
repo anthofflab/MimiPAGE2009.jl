85cfc9699d3a0119aa30a59b431664a7c5d922c1
James Rising <jarising@gmail.com>
2016-08-19 11:29:34 -0700

    add ref

--- src/TotalForcing.jl
+++ src/TotalForcing.jl
@@ -13,6 +13,7 @@ end
 function run_timestep(s::TotalForcing, tt::Int64)
     v, p, d = getvpd(s)
 
+    # From equation 16 of Hope (2006)
     v.ft_totalforcing[tt] = p.f_CO2forcing[tt] + p.f_CH4forcing[tt] + p.f_N2Oforcing[tt] + p.f_lineargasforcing[tt] + p.exf_excessforcing[tt]
 end
 
