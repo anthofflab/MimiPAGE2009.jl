fdb675c5ad9a7ece5cb98a2dc5158a0dd1796979
James Rising <jarising@gmail.com>
2016-09-12 17:48:37 -0700

    fixed mean lat

--- test/test_radforcing.jl
+++ test/test_radforcing.jl
@@ -2,10 +2,10 @@ using DataFrames
 
 radforcing = readtable("../calibration/rcp85-radforcing.csv")
 
-# Pull out the particular years
-rows = convert(Vector{Bool}, map(year -> in(year, m.indices_values[:time]), radforcing[:, 1]))
-
 # Run the model
 include("../src/main_model.jl")
+
+# Pull out the particular years
+rows = convert(Vector{Bool}, map(year -> in(year, m.indices_values[:time]), radforcing[:, 1]))
 
 DataFrame(co2_estimated=m[:co2forcing, :f_CO2forcing], co2_observed=radforcing[rows, :CO2_RF])
