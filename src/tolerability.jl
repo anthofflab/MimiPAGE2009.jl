64cbeefb22981e52ac56d8cc51e1e59c52aea70a
Niklas <nicklollo2@gmail.com>
2016-09-19 22:09:21 -0700

    Added in data files and adjusted time sequence

--- src/tolerability.jl
+++ src/tolerability.jl
@@ -8,7 +8,7 @@
     rt_realizedtemperature = Parameter(index=[time, region], unit="degreeC")
 
     #component parameters
-    #uncertain parameter (tolerable before discontinuity)
+        #uncertain parameters (tolerable before discontinuity)
     tm_regionalmultiplier = Parameter()
     tr_0_tolerablerateofchange= Parameter()
     tp_0_tolerableplateau = Parameter()
@@ -39,8 +39,15 @@ function run_timestep(s::Tolerability, t::Int64)
         v.atp_adjustedtolerableplateau[t,r] = v.tp_tolerableplateau[r] + p.plat_increaseintolerableplateaufromadaptation[t,r]
         v.atr_adjustedtolerablerate[t,r] = v.tr_tolerablerateofchange[r] + p.slope_increaseintolerableratefromadaptation[t,r]
 
-        v.atl_adjustedtolerableleveloftemprise[t,r] = min[v.atp_adjustedtolerableplateau[t,r], v.atl_adjustedtolerableleveloftemprise[t-1,r] +
-            v.atr_adjustedtolerablerate[r]*(p.y_year[t]-p.y_year[t-1])]
+        if t==1
+            v.atl_adjustedtolerableleveloftemprise[t,r] = min[v.atp_adjustedtolerableplateau[t,r],
+                p.atl_0_adjustedtolerableplateau[r] +
+                v.atr_adjustedtolerablerate[r]*(p.y_year[t]-p.y_year[t-1])]
+        else
+            v.atl_adjustedtolerableleveloftemprise[t,r] = min[v.atp_adjustedtolerableplateau[t,r],
+                v.atl_adjustedtolerableleveloftemprise[t-1,r] +
+                v.atr_adjustedtolerablerate[r]*(p.y_year[t]-p.y_year[t-1])]
+        end
 
         v.i_regionalimpact[t,r] = max[0, p.rt_g_globaltemperature[t,r]- v.atl_adjustedtolerableleveloftemprise[t,r]]
     end
@@ -53,9 +60,8 @@ function addtolerabilitymarket(model::Model)
     tolerabilitymarketcomp[:tr_0_tolerablerateofchange] = 0.
     tolerabilitymarketcomp[:tp_0_tolerableplateau] = 2.
     tolerabilitymarketcomp[:atl_0_adjustedtolerableplateau] = 0.
-    tolerabilitymarketcomp[:slope_increaseintolerableratefromadaptation] = readfile("../data/market_slope.csv")
+    tolerabilitymarketcomp[:slope_increaseintolerableratefromadaptation] = readfile("../data/market_slope.csv") #per decade!
     tolerabilitymarketcomp[:plat_increaseintolerableplateaufromadaptation] = readfile("../data/market_plateau.csv")
-
 
     return tolerabilitymarketcomp
 end
@@ -67,8 +73,8 @@ function addtolerabilitynonmarket(model::Model)
     tolerabilitynonmarketcomp[:tr_0_tolerablerateofchange] = 0.
     tolerabilitynonmarketcomp[:tp_0_tolerableplateau] = 2.
     tolerabilitynonmarketcomp[:atl_0_adjustedtolerableplateau] = 0.
-    tolerabilitynonmarketcomp[:slope_increaseintolerableratefromadaptation] = readfile("../data/nonmarket_slope.csv")
-    tolerabilitynonmarketcomp[:plat_increaseintolerableplateaufromadaptation] = readfile("../data/market_plateau.csv")
+    tolerabilitynonmarketcomp[:slope_increaseintolerableratefromadaptation] = readfile("../data/nonmarket_slope.csv") #per decade!
+    tolerabilitynonmarketcomp[:plat_increaseintolerableplateaufromadaptation] = readfile("../data/nonmarket_plateau.csv")
 
 
     return tolerabilitynonmarketcomp
