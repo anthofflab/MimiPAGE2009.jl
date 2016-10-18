e621768f566b70525b3284ddf8722066f52428a4
Frances Moore <fmoore@ucdavis.edu>
2016-10-17 17:43:27 -0700

    fixed pow / ipow variables

--- src/MarketDamages.jl
+++ src/MarketDamages.jl
@@ -41,6 +41,7 @@
     igdp_ImpactatActualGDPperCap=Variable(index=[time, region])
     isat_ImpactinclSaturationandAdaptation= Variable(index=[time,region])
     isat_per_cap_ImpactperCapinclSaturationandAdaptation = Variable(index=[time,region])
+    pow_MarketImpactExponent=Variable()
 
 end
 
@@ -78,7 +79,7 @@ function run_timestep(s::MarketDamages, t::Int64)
         end
 
         v.iref_ImpactatReferenceGDPperCap[t,r]= p.WINCF_weightsfactor[r]*((p.W_MarketImpactsatCalibrationTemp + p.iben_MarketInitialBenefit * p.tcal_CalibrationTemp)*
-            (v.i_regionalimpact[t,r]/p.tcal_CalibrationTemp)^p.ipow_MarketImpactFxnExponent - v.i_regionalimpact[t,r] * p.iben_MarketInitialBenefit)
+            (v.i_regionalimpact[t,r]/p.tcal_CalibrationTemp)^p.pow_MarketImpactExponent - v.i_regionalimpact[t,r] * p.iben_MarketInitialBenefit)
 
         v.igdp_ImpactatActualGDPperCap[t,r]= v.iref_ImpactatReferenceGDPperCap[t,r]*
             (p.rgdp_per_cap_SLRRemainGDP[t,r]/p.GDP_per_cap_focus_0_FocusRegionEU)^p.ipow_MarketImpactFxnExponent
@@ -121,6 +122,7 @@ function addmarketdamages(model::Model)
     marketdamagescomp[:ipow_MarketImpactFxnExponent] = -.13
     marketdamagescomp[:SAVE_savingsrate]= 15.
     marketdamagescomp[:GDP_per_cap_focus_0_FocusRegionEU]= (1.39*10^7)/496
+    marketdamagescomp[:pow_MarketImpactExponent]=2.17
 
     return marketdamagescomp
 end
