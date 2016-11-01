9eb5400c0ef648016329f35ef46beaac68f59448
Cecilia <ceciliahspringer@gmail.com>
2016-10-31 17:33:30 -0700

    updating discontinuity component

--- src/Discontinuity.jl
+++ src/Discontinuity.jl
@@ -1,23 +1,28 @@
 using Mimi
 
 @defcomp Discontinuity begin
-  irefeqdis_discimpact=Variable(index=[region], unit="%")
+  irefeqdis_eqdiscimpact=Variable(index=[region], unit="%")
   wincf_weightsfactor=Parameter(index=[region], unit="unitless")
-  wdis_gdplostdisc=Variable(index=[region], unit="%")
-  igdpeqdis_discimpactactualgdp=Variable(index=[time,region], unit="%")
+  wdis_gdplostdisc=Parameter(unit="%")
 
-# end 10/24
-  rgdppercap_realgdppercapita=Parameter(index=[time,region], unit="\$") # probably wrong
+  igdpeqdis_eqdiscimpact=Variable(index=[time,region], unit="%")
+  rgdp_per_cap_NonMarketRemainGDP=Parameter(index=[time,region], unit="unitless")
+  ipow_incomeexponent=Parameter(unit="unitless")
 
-# old shit
-  rt_g_globaltemperature = Variable(index=[time], unit="degreeC") # Global mean realized temperature
-  tdis_tolerabledisc=Parameter(unit="degreeC")
-  wdis_GDPlostdisc=Variable(index=[region], unit="%")
-  wdis_0_GDPlostdisc=Parameter(index=[region], unit="%")
-  wf_weightsfactor=Parameter(index=[region], unit="unitless")
-  widis_weighteddiscimpact=Variable(index=[time, region], unit="\$M") # Note: discuss with group if we need to specify the price level of dollars
-  pdis_discprobability=Parameter(unit="%/degreeC")
-  gdp = Parameter(index=[time, region], unit="\$M")
+  igdp_realizeddiscimpact=Variable(index=[time,region], unit="%")
+  occurdis_occurrencedummy=Variable(index=[time], unit="unitless")
+  expfdis_discdecay=Variable(index=[time], unit="unitless")
+
+  y_year=Parameter(index=[time], unit="year")
+  distau_discontinuityexponent=Parameter(unit="unitless")
+
+  idis_lossfromdisc=Parameter(index=[time], unit="%") #could be wrong
+  pdis_probability=Parameter(unit="%/degreeC")
+
+  isat_satdiscimpact=Variable(index=[time,region], unit="%")
+  isatg_saturationmodification=Variable(unit="unitless")
+
+  # documentation includes per capita saturation impacts, but we did not include it here
 
 end
 
@@ -26,15 +31,29 @@ run_timestep(s::Discontinuity, tt::Int64)
   p = s.Parameters
   d = s.Dimensions
 
-  v.idis_discimpact[t] = max([0, (v.rt_g_globaltemperature[t] - p.tdis_tolerabledisc)])
-  v.wdis_GDPlostdisc[r] = min([1, ((p.wdis_0_GDPlostdisc[r]*p.wf_weightsfactor[r])/100)])
-  v.widis_weighteddiscimpact[t,r] = v.idis_discimpact[t] * (p.pdis_discprobability/100) * v.wdis_GDPlostdisc[r] * p.gdp[t,r]
+  for rr in d.region
+
+  v.irefeqdis_discimpact[r] = p.wincf_weightsfactor[r]*p.wdis_gdplostdisc
+
+  v.igdpeqdis_eqdiscimpact[i,r] = v.irefeqdis_discimpact[r] * (p.rgdp_per_cap_NonMarketRemainGDP[i,r]/p.rgdp_per_cap_NonMarketRemainGDP[i,1])^p.ipow_incomeexponent
+
+  # stopping here 10/31
+
+end
 
 
-  # for weights data:
+  # for weights data, if using the provided distributions:
   # using Distributions
   # x = TriangularDist(min, max, mode)    put in actual data
   # mean(x)
   # construct a vector of triangular distributions
   # data = [TriangularDist(46, 74, 60), Normal(4.,3.)]
   # mean.(data)
+
+function addDiscontinuity(model::Model)
+
+    disccomp = addcomponent(model, Discontinuity)
+
+    disccomp[:wincf_weightsfactor]=[0.8, 0.8, 0.4, 0.8, 0.8, 0.6, 0.6]
+    disccomp[:wdis_gdplostdisc]=0.15
+    disccomp[:ipow_incomeexponent]=-0.5
