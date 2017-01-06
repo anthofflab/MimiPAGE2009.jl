429e6cdf8e8af687dd0db9a3a667f55fc3ac5023
Cecilia <ceciliahspringer@gmail.com>
2017-01-06 13:33:33 -0600

    Updated discontinuity

--- test/test_Discontinuity.jl
+++ test/test_Discontinuity.jl
@@ -3,14 +3,16 @@ using Mimi
 include("../src/Discontinuity.jl")
 
 m = Model()
-setindex(m, :time, [2009, 2010, 2020, 2030, 2040, 2050, 2075, 2100, 2150, 2200])
+setindex(m, :time, 10)
 setindex(m, :region, ["EU", "USA", "OECD","USSR","China","SEAsia","Africa","LatAmerica"])
 
 addcomponent(m, Discontinuity)
 
 setparameter(m, :Discontinuity, :rt_g_globaltemperature, ones(10))
-setparameter(m, :Discontinuity, :y_year, [2009,2010,2020,2030,2040,2050,2075,2100,2150,2200]) 
-setparameter(m, :Discontinuity, :y_year_0, 2008)
+setparameter(m, :Discontinuity, :y_year_0, 2008.)
+setparameter(m, :Discontinuity, :y_year, [2009, 2010, 2020, 2030, 2040, 2050, 2075, 2100, 2150, 2200]) #real value
+setparameter(m, :Discontinuity, :wincf_weightsfactor, [1, 0.8, 0.8, 0.4, 0.8, 0.8, 0.6, 0.6])
+setparameter(m, :Discontinuity, :wdis_gdplostdisc, 0.15)
 
 ##running Model
 run(m)
