fa3d89082868d0e04bab2a3e43c89ad4fb0c82f2
Niklas Lollo <nicklollo2@gmail.com>
2017-02-07 13:14:58 -0800

    found y_year_0 bug

--- test/test_Population.jl
+++ test/test_Population.jl
@@ -9,14 +9,17 @@ setindex(m, :time, [2009, 2010, 2020, 2030, 2040, 2050, 2075, 2100, 2150, 2200])
 setindex(m, :region, ["EU", "USA", "OECD","USSR","China","SEAsia","Africa","LatAmerica"])
 
 population = addpopulation(m)
-
+#population[:y_year_0] = 2008.
 p = load_parameters(m)
-p["y_year_0"] = 2008.
-p["y_year"] = m.indices_values[:time]
+p[:y_year] = m.indices_values[:time]
+p[:y_year_0] = 2008.
 
 setleftoverparameters(m, p)
+#setparameter(m,:Population, :y_year_0, 2008.)
 
 run(m)
 
-uspop2100 = m[:Population, :pop_population][8, 2]
+pop = m[:Population, :pop_population]
+@test !isna(m[:Population, :pop_population])
+
 @test uspop2100 > 400 && uspop2100 < 500
