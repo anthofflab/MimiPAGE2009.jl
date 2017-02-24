aabdd8fdc5eaef19fe93a2ba96ecd911e2b7ca4b
David Anthoff <anthoff@berkeley.edu>
2017-02-24 15:08:29 -0800

    Import Base.test in runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,5 @@
+using Base.Test
+
 tests = ["ClimateTemperature","CH4cycle","CO2emissions","CH4emissions","CO2forcing","N2Oemissions","N2Oforcing","N2Ocycle", "CH4forcing","TotalForcing", "SulphateForcing","CO2cycle", "AdaptationCosts", "loadparameters", "EquityWeighting", "mainmodel"]
 
 for t in tests
