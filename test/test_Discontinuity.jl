1c20976823acb34236dfa5c472b22492c5f2d362
Frances Moore <fmoore@ucdavis.edu>
2017-02-28 17:24:36 -0800

    added data to discontinuity test

--- test/test_Discontinuity.jl
+++ test/test_Discontinuity.jl
@@ -4,16 +4,16 @@ using Base.Test
 include("../src/Discontinuity.jl")
 
 m = Model()
-setindex(m, :time, 10)
+setindex(m, :time, [2009, 2010, 2020, 2030, 2040, 2050, 2075, 2100, 2150, 2200])
 setindex(m, :region, ["EU", "USA", "OECD","USSR","China","SEAsia","Africa","LatAmerica"])
 
 adddiscontinuity(m)
 
-setparameter(m, :Discontinuity, :rt_g_globaltemperature, [0.75,0.75,1.0,1.1,1.3,1.5,2.0,2.6,3.0,4.5])
+setparameter(m, :Discontinuity, :rt_g_globaltemperature, [0.75,0.77,0.99,1.27,1.62,1.99,3.07,3.90,5.10,6.03])
 setparameter(m, :Discontinuity, :y_year_0, 2008.)
 setparameter(m, :Discontinuity, :y_year, [2009, 2010, 2020, 2030, 2040, 2050, 2075, 2100, 2150, 2200])
-setparameter(m, :Discontinuity, :rgdp_per_cap_NonMarketRemainGDP, transpose(repmat(readpagedata(m, "../data/gdp_0.csv"), 1, 10))/transpose(repmat(readpagedata(m, "../data/pop0_initpopulation.csv"), 1, 10)))
-setparameter(m, :Discontinuity, :rcons_per_cap_NonMarketRemainConsumption, ones(10,8))
+setparameter(m, :Discontinuity, :rgdp_per_cap_NonMarketRemainGDP, readpagedata(m, "../test/validationdata/rgdp_per_cap_NonMarketRemainGDP.csv"))
+setparameter(m, :Discontinuity, :rcons_per_cap_NonMarketRemainConsumption, readpagedata(m, "../test/validationdata/rcons_per_cap_NonMarketRemainConsumption.csv"))
 
 ##running Model
 run(m)
