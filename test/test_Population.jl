add969ab4922d98a2a7a7c08ad66e45f80521599
James Rising <jarising@gmail.com>
2017-02-28 18:39:30 -0800

    made population perfect

--- test/test_Population.jl
+++ test/test_Population.jl
@@ -5,18 +5,24 @@ include("../src/load_parameters.jl")
 include("../src/Population.jl")
 
 m = Model()
-setindex(m, :time, [2009, 2010, 2020, 2030, 2040, 2050, 2075, 2100, 2150, 2200])
+setindex(m, :time, convert(Vector{Float64}, [2009, 2010, 2020, 2030, 2040, 2050, 2075, 2100, 2150, 2200]))
 setindex(m, :region, ["EU", "USA", "OECD","USSR","China","SEAsia","Africa","LatAmerica"])
 
 population = addpopulation(m)
-#population[:y_year_0] = 2008.
+population[:y_year_0] = 2008.
+population[:y_year] = m.indices_values[:time]
+
 p = load_parameters(m)
-p[:y_year] = m.indices_values[:time]
-p["y_year_0"] = 2008.
 
 setleftoverparameters(m, p)
 #setparameter(m,:Population, :y_year_0, 2008.)
 
 run(m)
 
+# Generated data
 pop = m[:Population, :pop_population]
+
+# Recorded data
+pop_compare = readpagedata(m, "validationdata/pop_population.csv")
+
+@test_approx_eq_eps pop pop_compare 1e-3
