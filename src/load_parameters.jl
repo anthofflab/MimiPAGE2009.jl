d74f9f12180533dc95159294578fde9f8961fb67
James Rising <jarising@gmail.com>
2017-03-22 15:57:25 -0400

    Handling of relative paths

--- src/load_parameters.jl
+++ src/load_parameters.jl
@@ -25,6 +25,11 @@ function checktimeorder(model::Model, times, file)
 end
 
 function readpagedata(model::Model, filepath::AbstractString)
+    # Handle relative paths
+    if filepath[1] ∉ ['.', '/'] && !isfile(filepath)
+        filepath = joinpath(dirname(@__FILE__), "..", filepath)
+    end
+
     content = readlines(filepath)
 
     firstline = chomp(content[1])
