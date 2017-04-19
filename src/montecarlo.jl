4d6421b7c3ddecd4e7e4a358d036bb563f4f2c4d
Frances Moore <fmoore@ucdavis.edu>
2017-04-18 18:08:32 -0700

    improved syntax

--- src/montecarlo.jl
+++ src/montecarlo.jl
@@ -3,17 +3,17 @@ using Distributions
 include("getpagefunction.jl")
 getpage()
 run(m)
-temp=m[:ClimateTemperature,:rt_g_globaltemperature][10]
+temp=[m[:ClimateTemperature,:rt_g_globaltemperature][10]]
 
 #Define uncertin parameters based on PAGE 2009 documentation
 uncertain_params=Dict()
-uncertain_params[:res_CO2atmlifetime]=TriangularDist(50., 100., 70.)
+uncertain_params[:co2cycle,:res_CO2atmlifetime]=TriangularDist(50., 100., 70.)
 
-nit=2
-for i in 1:nit
-    for (p_name, p_dist) in uncertain_params
+nit=50
+@time for i in 1:nit
+    for ((p_comp, p_name), p_dist) in uncertain_params
         v = rand(p_dist)
-        setparameter(m, p_name, v)
+        setparameter(m, p_comp,p_name, v)
     end
     run(m)
     push!(temp, m[:ClimateTemperature,:rt_g_globaltemperature][10])
