cf139ac62a4d3e9953e7be5b9514cbf7698c8e4d
Frances Moore <fmoore@ucdavis.edu>
2017-05-09 15:54:11 -0700

    additional output variables

--- src/montecarlo.jl
+++ src/montecarlo.jl
@@ -8,12 +8,21 @@ include("getpagefunction.jl")
 m = getpage()
 run(m)
 
-nit=1000;
+nit=100000;
 td=zeros(nit);
 tpc=zeros(nit);
 tac=zeros(nit);
 te=zeros(nit);
-@time for i in 1:nit
+ft=zeros(nit);
+rt_g=zeros(nit);
+s=zeros(nit);
+c_co2concentration=zeros(nit);
+rgdppercap_slr=zeros(nit);
+rgdppercap_market=zeros(nit);
+rgdppercap_nonmarket=zeros(nit);
+rgdppercap_disc=zeros(nit);
+
+for i in 1:nit
     # Randomize all components with random parameters
     randomizeCO2cycle(m)
     randomizesulphatecomp(m)
@@ -50,9 +59,17 @@ te=zeros(nit);
     tpc[i]=m[:EquityWeighting,:tpc_totalaggregatedcosts]
     tac[i]=m[:EquityWeighting,:tac_totaladaptationcosts]
     te[i]=m[:EquityWeighting,:te_totaleffect]
+    c_co2concentration[i]=m[:co2cycle,:c_CO2concentration][10]
+    ft[i]=m[:TotalForcing,:ft_totalforcing][10]
+    rt_g[i]=m[:ClimateTemperature,:rt_g_globaltemperature][10]
+    s[i]=m[:SeaLevelRise,:s_sealevel][10]
+    rgdppercap_slr[i]=m[:SLRDamages,:rgdp_per_cap_SLRRemainGDP][10,8]
+    rgdppercap_market[i]=m[:MarketDamages,:rgdp_per_cap_MarketRemainGDP][10,8]
+    rgdppercap_nonmarket[i]=m[:NonMarketDamages,:rgdp_per_cap_NonMarketRemainGDP][10,8]
+    rgdppercap_disc[i]=m[:Discontinuity,:rgdp_per_cap_NonMarketRemainGDP][10,8]
 end
 
-df=DataFrame(td=td,tpc=tpc,tac=tac,te=te)
+df=DataFrame(td=td,tpc=tpc,tac=tac,te=te,c_co2concentration=c_co2concentration,ft=ft,rt_g=rt_g,sealevel=s,rgdppercap_slr=rgdppercap_slr,rgdppercap_market=rgdppercap_market,rgdppercap_nonmarket=rgdppercap_nonmarket,rgdppercap_di=rgdppercap_disc)
 writetable("../test/validationdata/mimipagemontecarlooutput.csv",df)
 #make plots of output Distributions
 R"x11()"
