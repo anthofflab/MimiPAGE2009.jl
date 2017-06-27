85d8d13469e0c48b38ceef2c48c800727467efb1
James Rising <jarising@gmail.com>
2017-06-27 22:47:47 +0200

    optimize er factors

--- src/getpagefunction.jl
+++ src/getpagefunction.jl
@@ -28,11 +28,7 @@ include("TotalAdaptationCosts.jl")
 include("Population.jl")
 include("EquityWeighting.jl")
 
-function buildpage()
-    m = Model()
-    setindex(m, :time, [2009, 2010, 2020, 2030, 2040, 2050, 2075, 2100, 2150, 2200])
-    setindex(m, :region, ["EU", "USA", "OECD","USSR","China","SEAsia","Africa","LatAmerica"])
-
+function buildpage(m::Model)
     #add all the components
     CO2emissions = addcomponent(m,co2emissions)
     CO2cycle = addCO2cycle(m)
@@ -172,7 +168,7 @@ function buildpage()
     return m
 end
 
-function initpage(m)
+function initpage(m::Model)
     p = load_parameters(m)
     p["y_year_0"] = 2008.
     p["y_year"] = m.indices_values[:time]
@@ -180,7 +176,11 @@ function initpage(m)
 end
 
 function getpage()
-    m = buildpage()
+    m = Model()
+    setindex(m, :time, [2009, 2010, 2020, 2030, 2040, 2050, 2075, 2100, 2150, 2200])
+    setindex(m, :region, ["EU", "USA", "OECD","USSR","China","SEAsia","Africa","LatAmerica"])
+
+    buildpage(m)
 
     # next: add vector and panel example
     initpage(m)
--- src/optimize.jl
+++ src/optimize.jl
@@ -1,62 +1,73 @@
 using Mimi
 using OptiMimi
-
 include("getpagefunction.jl")
-m = buildpage()
+
+# Create the model
+m = Model()
+setindex(m, :time, [2009, 2010, 2020, 2030, 2040, 2050, 2075, 2100, 2150, 2200])
+setindex(m, :region, ["EU", "USA", "OECD","USSR","China","SEAsia","Africa","LatAmerica"])
 
 @defcomp AbatementScale begin
-    q0propinit = Parameter(unit="% of BAU emissions")
+    region=Index()
 
-    co2_q0propinit = Variable(unit="% of BAU emissions")
-    ch4_q0propinit = Variable(unit="% of BAU emissions")
-    n2o_q0propinit = Variable(unit="% of BAU emissions")
-    lin_q0propinit = Variable(unit="% of BAU emissions")
+    emissiongrowthfactor = Parameter(unit="% of normal growth")
+
+    er_CO2emissionsgrowth = Parameter(index=[time,region],unit="%")
+    er_CH4emissionsgrowth = Parameter(index=[time,region],unit="%")
+    er_N2Oemissionsgrowth = Parameter(index=[time,region],unit="%")
+    er_LGemissionsgrowth = Parameter(index=[time,region],unit="%")
+
+    er_CO2emissionsgrowth_new = Variable(index=[time,region],unit="%")
+    er_CH4emissionsgrowth_new = Variable(index=[time,region],unit="%")
+    er_N2Oemissionsgrowth_new = Variable(index=[time,region],unit="%")
+    er_LGemissionsgrowth_new = Variable(index=[time,region],unit="%")
 end
 
-function init(s::AbatementScale)
+function run_timestep(s::AbatementScale, tt::Int64)
     v = s.Variables
     p = s.Parameters
     d = s.Dimensions
 
-    v.co2_q0propinit = v.ch4_q0propinit = v.n2o_q0propinit = v.lin_q0propinit = p.q0propinit
+    for rr in d.region
+        v.er_CO2emissionsgrowth_new[tt, rr] = p.er_CO2emissionsgrowth[tt, rr] * p.emissiongrowthfactor / 100
+        v.er_CH4emissionsgrowth_new[tt, rr] = p.er_CH4emissionsgrowth[tt, rr] * p.emissiongrowthfactor / 100
+        v.er_N2Oemissionsgrowth_new[tt, rr] = p.er_N2Oemissionsgrowth[tt, rr] * p.emissiongrowthfactor / 100
+        v.er_LGemissionsgrowth_new[tt, rr] = p.er_LGemissionsgrowth[tt, rr] * p.emissiongrowthfactor / 100
+    end
 end
 
-function run_timestep(s::AbatementScale, t::Int64)
-    v = s.Variables
-    p = s.Parameters
-    d = s.Dimensions
+addcomponent(m, AbatementScale)
 
-    v.co2_q0propinit = v.ch4_q0propinit = v.n2o_q0propinit = v.lin_q0propinit = p.q0propinit
-end
+buildpage(m)
 
-abatementscale = addcomponent(m, AbatementScale)
-connectparameter(m, :AbatementCostsCO2, :q0propinit_CutbacksinNegativeCostinFocusRegioninBaseYear, :AbatementScale, :co2_q0propinit)
-connectparameter(m, :AbatementCostsCH4, :q0propinit_CutbacksinNegativeCostinFocusRegioninBaseYear, :AbatementScale, :ch4_q0propinit)
-connectparameter(m, :AbatementCostsN2O, :q0propinit_CutbacksinNegativeCostinFocusRegioninBaseYear, :AbatementScale, :n2o_q0propinit)
-connectparameter(m, :AbatementCostsLin, :q0propinit_CutbacksinNegativeCostinFocusRegioninBaseYear, :AbatementScale, :lin_q0propinit)
+connectparameter(m, :co2emissions, :er_CO2emissionsgrowth, :AbatementScale, :er_CO2emissionsgrowth_new)
+connectparameter(m, :ch4emissions, :er_CH4emissionsgrowth, :AbatementScale, :er_CH4emissionsgrowth_new)
+connectparameter(m, :n2oemissions, :er_N2Oemissionsgrowth, :AbatementScale, :er_N2Oemissionsgrowth_new)
+connectparameter(m, :LGemissions, :er_LGemissionsgrowth, :AbatementScale, :er_LGemissionsgrowth_new)
 
 # Initial value of q0propinit
-setparameter(m, :AbatementScale, :q0propinit, 10.)
+setparameter(m, :AbatementScale, :emissiongrowthfactor, 90.)
 
 initpage(m)
 run(m)
 
-#objective(model::Model) = -model[:EquityWeighting, :te_totaleffect]
+bestpolicy(model::Model) = -model[:EquityWeighting, :te_totaleffect]
 
-function bestpolicy(model::Model)
-    -model[:EquityWeighting, :te_totaleffect]
-end
+optprob = problem(m, [:AbatementScale], [:emissiongrowthfactor], [0.], [100.0], bestpolicy);
+(maxf, maxx) = solution(optprob, () -> [90.])
 
-optprob = problem(m, [:AbatementScale], [:q0propinit], [0.], [100.0], bestpolicy)
-(maxf, maxx) = solution(optprob, () -> [10.])
+# (-3.006562326528711e7,[0.0])
 
-# (-2.04735392060811e8,[100.0])
+worstpolicy(model::Model) = model[:EquityWeighting, :te_totaleffect]
 
-function worstpolicy(model::Model)
-    model[:EquityWeighting, :te_totaleffect]
-end
+optprob = problem(m, [:AbatementScale], [:emissiongrowthfactor], [0.], [100.0], worstpolicy);
+(maxf, maxx) = solution(optprob, () -> [90.])
 
-optprob = problem(m, [:AbatementScale], [:q0propinit], [0.], [100.0], worstpolicy)
-(maxf, maxx) = solution(optprob, () -> [10.])
+# (2.1320245992205706e8,[100.0])
 
-# (2.373957504679534e8,[0.0])
+erfactors = convert(Vector{Float64}, linspace(0, 100, 100))
+results = sensitivity(m, :AbatementScale, :emissiongrowthfactor, model -> model[:EquityWeighting, :te_totaleffect], erfactors)
+
+using Plots
+plotly()
+plot(erfactors, results, xlab="Emissions Growth Factor (%)", ylab="Total Effect")
