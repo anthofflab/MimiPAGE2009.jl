1b2ecc82288b2a90d13583952a551b28aef163d4
James Rising <jarising@gmail.com>
2017-07-02 16:44:10 -0400

    working optimization code

--- src/optimize-single.jl
+++ src/optimize-single.jl
@@ -10,7 +10,7 @@ setindex(m, :region, ["EU", "USA", "OECD","USSR","China","SEAsia","Africa","LatA
 @defcomp AbatementScale begin
     region=Index()
 
-    emissiongrowthfactor = Parameter(index=[time], unit="% of normal growth")
+    emissiongrowthfactor = Parameter(unit="% of normal growth")
 
     er_CO2emissionsgrowth = Parameter(index=[time,region],unit="%")
     er_CH4emissionsgrowth = Parameter(index=[time,region],unit="%")
@@ -29,10 +29,10 @@ function run_timestep(s::AbatementScale, tt::Int64)
     d = s.Dimensions
 
     for rr in d.region
-        v.er_CO2emissionsgrowth_new[tt, rr] = p.er_CO2emissionsgrowth[tt, rr] * p.emissiongrowthfactor[tt] / 100
-        v.er_CH4emissionsgrowth_new[tt, rr] = p.er_CH4emissionsgrowth[tt, rr] * p.emissiongrowthfactor[tt] / 100
-        v.er_N2Oemissionsgrowth_new[tt, rr] = p.er_N2Oemissionsgrowth[tt, rr] * p.emissiongrowthfactor[tt] / 100
-        v.er_LGemissionsgrowth_new[tt, rr] = p.er_LGemissionsgrowth[tt, rr] * p.emissiongrowthfactor[tt] / 100
+        v.er_CO2emissionsgrowth_new[tt, rr] = p.er_CO2emissionsgrowth[tt, rr] * p.emissiongrowthfactor / 100
+        v.er_CH4emissionsgrowth_new[tt, rr] = p.er_CH4emissionsgrowth[tt, rr] * p.emissiongrowthfactor / 100
+        v.er_N2Oemissionsgrowth_new[tt, rr] = p.er_N2Oemissionsgrowth[tt, rr] * p.emissiongrowthfactor / 100
+        v.er_LGemissionsgrowth_new[tt, rr] = p.er_LGemissionsgrowth[tt, rr] * p.emissiongrowthfactor / 100
     end
 end
 
@@ -51,26 +51,30 @@ connectparameter(m, :AbatementCostsN2O, :er_emissionsgrowth, :AbatementScale, :e
 connectparameter(m, :AbatementCostsLin, :er_emissionsgrowth, :AbatementScale, :er_LGemissionsgrowth_new)
 
 # Initial value of q0propinit
-setparameter(m, :AbatementScale, :emissiongrowthfactor, repmat([100.], 10))
+setparameter(m, :AbatementScale, :emissiongrowthfactor, 90.)
 
 initpage(m)
 run(m)
 
 bestpolicy(model::Model) = -model[:EquityWeighting, :te_totaleffect]
 
-optprob = problem(m, [:AbatementScale], [:emissiongrowthfactor], repmat([0.], 10), repmat([100.0], 10), bestpolicy);
-(maxf, maxx) = solution(optprob, () -> repmat([90.], 10))
+optprob = problem(m, [:AbatementScale], [:emissiongrowthfactor], [0.], [100.0], bestpolicy);
+(maxf, maxx) = solution(optprob, () -> [90.])
 
 # Uniform across all gases
+# Single value
+# (-9.281691347523426e7,[75.914])
 # Across all periods
 # (-6.263442759850269e7,[85.709,84.6415,84.3794,79.9315,79.9212,73.3482,58.1531,33.0268,2.94466e-20,4.91144e-20])
 
 worstpolicy(model::Model) = model[:EquityWeighting, :te_totaleffect]
 
 optprob = problem(m, [:AbatementScale], [:emissiongrowthfactor], [0.], [100.0], worstpolicy);
-(maxf, maxx) = solution(optprob, () -> repmat([90.], 10))
+(maxf, maxx) = solution(optprob, () -> [90.])
 
 # Uniform across all gases
+# Single value
+# (2.1320245992205706e8,[100.0])
 # Across all periods
 # (2.1320245992205706e8,[100.0,100.0,100.0,100.0,100.0,100.0,100.0,100.0,100.0,100.0])
 
