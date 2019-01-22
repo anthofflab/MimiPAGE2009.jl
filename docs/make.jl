3c9de0066a6c13a5cd2fbaf1a70908f7661de237
David Anthoff <anthoff@berkeley.edu>
2019-01-22 11:38:14 -0800

    Update docs to Documenter.jl v0.21

--- docs/make.jl
+++ docs/make.jl
@@ -1,8 +1,7 @@
 using Documenter
 
 makedocs(
-	format = :html,
-	sitename = "mimi-page.jl",
+	sitename = "mimi-page-2009.jl",
 	pages = [
 		"Home" => "index.md",
 		"Getting started" => "gettingstarted.md",
@@ -12,9 +11,5 @@ makedocs(
 )
 
 deploydocs(
-    deps = nothing,
-    make = nothing,
-	target = "build",
-    repo = "github.com/anthofflab/mimi-page.jl.git",
-    julia = "0.6"
+    repo = "github.com/anthofflab/mimi-page-2009.jl.git"
 )
