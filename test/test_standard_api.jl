31975f9eaf096b5575e7df7de5ed8d58e65c8854
Cora Kingdon <ckingdon@berkeley.edu>
2020-03-05 10:24:03 -0500

    add test

--- test/test_standard_api.jl
+++ test/test_standard_api.jl
@@ -26,4 +26,12 @@ result = MimiPAGE2009.compute_scc_mm(year=2050)
 @test result.scc < scc1
 @test result.mm isa Mimi.MarginalModel
 
+# Test Monte Carlo SCC support
+sccs1 = MimiPAGE2009.compute_scc(year=2020, n=10, seed=350)
+sccs2 = MimiPAGE2009.compute_scc(year=2020, n=10, seed=350)
+@test sccs1 == sccs2
+
+sccs3 = MimiPAGE2009.compute_scc(year=2020, n=10, seed=351)
+@test sccs3 != sccs1
+
 end
