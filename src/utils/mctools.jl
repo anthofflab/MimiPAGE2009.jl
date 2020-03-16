e868ff3f66bed7ddf36013668c531f513c00fdaf
Cora Kingdon <ckingdon@berkeley.edu>
2020-03-16 10:14:30 -0400

    reorganize setting shared parameters

--- src/utils/mctools.jl
+++ src/utils/mctools.jl
@@ -6,16 +6,7 @@ and connect parameter `name` within `component` to this distinct global paramete
 """
 function setdistinctparameter(m::Model, component::Symbol, name::Symbol, value)
     globalname = Symbol(string(component, '_', name))
-    # @info "setdistinctparameter: globalname=$globalname"
-
-    param_dims = Mimi.parameter_dimensions(m, component, name)    
-
-    Mimi.set_external_param!(m, globalname, value; param_dims = param_dims)
-    
-    # Added keywd arg to bypass checking labels
-    connect_param!(m.md, component, name, globalname)
-    
-    nothing
+    set_param!(m, component, name, globalname, value)
 end
 
 """
