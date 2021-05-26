1784f5f02b39dc8c84d4302bf448c28e432e97e1
lrennels <lrennels@berkeley.edu>
2021-05-26 10:20:39 -0700

    mcs formatting

--- src/mcs.jl
+++ src/mcs.jl
@@ -11,14 +11,20 @@ function getsim()
 
                 # each component should have the same value for its save_savingsrate,
                 # so we use an RV here because in the model this is not an explicitly
-                # shared parameter, then assign below in component section
+                # shared parameter, then assign to components
                 rv(RV_save_savingsrate) = TriangularDist(10, 20, 15)
+                GDP.save_savingsrate = RV_save_savingsrate
+                MarketDamages.save_savingsrate = RV_save_savingsrate
+                NonMarketDamages.save_savingsrate = RV_save_savingsrate
+                SLRDamages.save_savingsrate = RV_save_savingsrate
 
                 # each component should have the same value for its tcal_CalibrationTemp
                 # so we use an RV here because in the model this is not an explicitly
-                # shared parameter, then assign below in component section
+                # shared parameter, then assign to components
                 rv(RV_tcal_CalibrationTemp) = TriangularDist(2.5, 3.5, 3.)
-                
+                MarketDamages.tcal_CalibrationTemp = RV_tcal_CalibrationTemp
+                NonMarketDamages.tcal_CalibrationTemp = RV_tcal_CalibrationTemp
+
                 # CO2cycle
                 co2cycle.air_CO2fractioninatm = TriangularDist(57, 67, 62)
                 co2cycle.res_CO2atmlifetime = TriangularDist(50, 100, 70)
@@ -43,27 +49,21 @@ function getsim()
                 SeaLevelRise.sltau_SLresponsetime = TriangularDist(500, 1500, 1000)
                 
                 # GDP
-                GDP.save_savingsrate = RV_save_savingsrate
                 GDP.isat0_initialimpactfxnsaturation = TriangularDist(20, 50, 30) 
                 
                 # MarketDamages
-                MarketDamages.save_savingsrate = RV_save_savingsrate
-                MarketDamages.tcal_CalibrationTemp = RV_tcal_CalibrationTemp
                 MarketDamages.iben_MarketInitialBenefit = TriangularDist(0, .3, .1)
                 MarketDamages.W_MarketImpactsatCalibrationTemp = TriangularDist(.2, .8, .5)
                 MarketDamages.pow_MarketImpactExponent = TriangularDist(1.5, 3, 2)
                 MarketDamages.ipow_MarketIncomeFxnExponent = TriangularDist(-.3, 0, -.1)
 
                 # NonMarketDamages
-                NonMarketDamages.save_savingsrate = RV_save_savingsrate
-                NonMarketDamages.tcal_CalibrationTemp = RV_tcal_CalibrationTemp
                 NonMarketDamages.iben_NonMarketInitialBenefit = TriangularDist(0, .2, .05)
                 NonMarketDamages.w_NonImpactsatCalibrationTemp = TriangularDist(.1, 1, .5)
                 NonMarketDamages.pow_NonMarketExponent = TriangularDist(1.5, 3, 2)
                 NonMarketDamages.ipow_NonMarketIncomeFxnExponent = TriangularDist(-.2, .2, 0)
                 
                 # SLRDamages
-                SLRDamages.save_savingsrate = RV_save_savingsrate
                 SLRDamages.scal_calibrationSLR = TriangularDist(0.45, 0.55, .5)
                 #SLRDamages.iben_SLRInitialBenefit = TriangularDist(0, 0, 0) # only usable if lb <> ub
                 SLRDamages.W_SatCalibrationSLR = TriangularDist(.5, 1.5, 1)
@@ -127,14 +127,6 @@ function getsim()
                 cmaxf_maxcostfactor["Africa"] = TriangularDist(1,1.5,1.2)
                 cmaxf_maxcostfactor["LatAmerica"] = TriangularDist(0.4,1.0,0.7)
                 
-                q0propmult_cutbacksatnegativecostinfinalyear = TriangularDist(0.3,1.2,0.7)
-                qmax_minus_q0propmult_maxcutbacksatpositivecostinfinalyear = TriangularDist(1,1.5,1.3)
-                c0mult_mostnegativecostinfinalyear = TriangularDist(0.5,1.2,0.8)
-                curve_below_curvatureofMACcurvebelowzerocost = TriangularDist(0.25,0.8,0.45)
-                curve_above_curvatureofMACcurveabovezerocost = TriangularDist(0.1,0.7,0.4)
-                cross_experiencecrossoverratio = TriangularDist(0.1,0.3,0.2)
-                learn_learningrate = TriangularDist(0.05,0.35,0.2)
-                
                 cf_costregional["USA"] = TriangularDist(0.6, 1, 0.8)
                 cf_costregional["OECD"] = TriangularDist(0.4, 1.2, 0.8)
                 cf_costregional["USSR"] = TriangularDist(0.2, 0.6, 0.4)
@@ -143,12 +135,20 @@ function getsim()
                 cf_costregional["Africa"] = TriangularDist(0.4, 0.8, 0.6)
                 cf_costregional["LatAmerica"] = TriangularDist(0.4, 0.8, 0.6)
 
+                # Other
+                q0propmult_cutbacksatnegativecostinfinalyear = TriangularDist(0.3,1.2,0.7)
+                qmax_minus_q0propmult_maxcutbacksatpositivecostinfinalyear = TriangularDist(1,1.5,1.3)
+                c0mult_mostnegativecostinfinalyear = TriangularDist(0.5,1.2,0.8)
+                curve_below_curvatureofMACcurvebelowzerocost = TriangularDist(0.25,0.8,0.45)
+                curve_above_curvatureofMACcurveabovezerocost = TriangularDist(0.1,0.7,0.4)
+                cross_experiencecrossoverratio = TriangularDist(0.1,0.3,0.2)
+                learn_learningrate = TriangularDist(0.05,0.35,0.2)
+
                 # NOTE: the below can probably be resolved into unique, unshared parameters with the same name
                 # in the new Mimi paradigm of shared and unshared parameters, but for now this will 
                 # continue to work!
 
                 # AbatementCosts
-
                 AbatementCostParametersCO2_emit_UncertaintyinBAUEmissFactorinFocusRegioninFinalYear = TriangularDist(-50,75,0)
                 AbatementCostParametersCH4_emit_UncertaintyinBAUEmissFactorinFocusRegioninFinalYear = TriangularDist(-25,100,0)
                 AbatementCostParametersN2O_emit_UncertaintyinBAUEmissFactorinFocusRegioninFinalYear = TriangularDist(-50,50,0)
@@ -180,7 +180,6 @@ function getsim()
                 AbatementCostParametersLin_ies_InitialExperienceStockofCutbacks = TriangularDist(1500,2500,2000)
 
                 # AdaptationCosts
-
                 AdaptiveCostsSeaLevel_cp_costplateau_eu = TriangularDist(0.01, 0.04, 0.02)
                 AdaptiveCostsSeaLevel_ci_costimpact_eu = TriangularDist(0.0005, 0.002, 0.001)
                 AdaptiveCostsEconomic_cp_costplateau_eu = TriangularDist(0.005, 0.02, 0.01)
